ec73268fa119833a7bf13abe122544161d786ac5
Jaheen <146910613+Jaheen27@users.noreply.github.com>
2026-08-18 23:34:19 +0530

    Add sol1: 15. 3Sum (C)

---
+++ problems/0015-3Sum/sol1.c
@@ -0,0 +1,22 @@
+// ==========================================================
+// 15. 3Sum
+// Difficulty : Medium
+// Language   : C
+// Solution   : #1
+// Runtime    : 37 ms (Beats 86%)
+// Memory     : 36.5 MB (Beats 85%)
+// Link       : https://leetcode.com/problems/3sum/
+// ==========================================================
+
+
+int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+
+    return (*(int *)a - *(int *)b);
+}
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free
+ ().
+ */
+ int compare(const void *a, const void *b) {
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+/**
