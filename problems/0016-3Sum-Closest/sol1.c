0c5c85771cea060f3805f204bda174d1a22434c0
Jaheen <146910613+Jaheen27@users.noreply.github.com>
2026-08-19 23:17:13 +0530

    Add sol1: 16. 3Sum Closest (C)

---
+++ problems/0016-3Sum-Closest/sol1.c
@@ -0,0 +1,22 @@
+// ==========================================================
+// 16. 3Sum Closest
+// Difficulty : Medium
+// Language   : C
+// Solution   : #1
+// Runtime    : 16 ms (Beats 35%)
+// Memory     : 9 MB (Beats 58%)
+// Link       : https://leetcode.com/problems/3sum-closest/
+// ==========================================================
+
+    int closest = nums[0] + nums[1] + nums[2];
+
+    qsort(nums, numsSize, sizeof(int), compare);
+int threeSumClosest(int* nums, int numsSize, int target)
+{
+
+}
+    return (*(int *)a - *(int *)b);
+{
+int compare(const void *a, const void *b)
+
+    for (int i = 0; i < numsSize - 2; i++)
